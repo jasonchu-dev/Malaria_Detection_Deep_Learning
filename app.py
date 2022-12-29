5b4abcfa2e1f85f7aaae7975092fa183cec0b50d
jasonchu-dev <noreply@github.com>
2022-12-29 00:29:30 -0800

    need to display image on page

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
+import cv2 as cv
+import numpy as np
+import tensorflow as tf
+import os
 
 app = Flask(__name__)
+model = tf.keras.models.load_model('model.h5')
 
 @app.route('/', methods=['GET'])
 def home():
@@ -8,7 +13,30 @@ def home():
 
 @app.route('/', methods=['POST'])
 def predict():
-    return render_template('index.html')
+    file = request.files['file']
+    if not file: return render_template('index.html')
+    path = './samples/' + file.filename
+    file.save(path)
+
+    img = cv.imread(path)
+    img = cv.resize(img, (225, 225))
+    img = img.reshape(1, 225, 225, 3)
+    img = img/255
+    nodes = model.predict(img)
+    # nodes is np.array within array and cannot flatten so it must be [0][0] and [0][1] not [0] and [1]
+    a = np.argmax(nodes, axis=1)
+    percent, result = None, None
+
+    if a == 0: 
+        result = 'Infected'
+        percent = nodes[0][0] * 100
+    else: 
+        result = 'Uninfected'
+        percent = nodes[0][1] * 100
+
+    imagename = os.listdir(os.getcwd() + r'\samples')[-1]
+
+    return render_template('index.html', result=result, percent=percent, imagename=imagename)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
